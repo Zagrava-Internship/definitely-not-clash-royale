21f21668c22ccf86c8ec8cb926262da538f50aed
Vladdjuga <vladdjuga@gmail.com>
2025-07-11 17:51:41 +0300

    feat: add UnitAnimator and UnitAnimationParameters for improved animation control

---
+++ Assets/Scripts/Units/Animation/UnitAnimationParameters.cs
@@ -0,0 +1,10 @@
+﻿namespace Units.Animation
+{
+    public static class UnitAnimationParameters
+    {
+        public const string IsMoving = "isMoving";
+        public const string IsAttacking = "isAttacking";
+        public const string MoveX = "moveX";
+        public const string MoveY = "moveY";
+    }
+}
---
+++ Assets/Scripts/Units/Animation/UnitAnimator.cs
@@ -0,0 +1,43 @@
+﻿using UnityEngine;
+
+namespace Units.Animation
+{
+    public class UnitAnimator:MonoBehaviour
+    {
+        private Animator _animator;
+
+        private void Awake()
+        {
+            _animator = GetComponent<Animator>();
+        }
+        // ResetState is used to reset the animator state when changing states or when the unit is idle
+        public void ResetState()
+        {
+            _animator.SetBool(UnitAnimationParameters.IsMoving, false);
+            _animator.SetBool(UnitAnimationParameters.IsAttacking, false);
+        }
+        // PlayMove is used to play the moving animation with the given direction
+        public void PlayMove(Vector2 direction)
+        {
+            _animator.SetBool(UnitAnimationParameters.IsMoving, true);
+            _animator.SetFloat(UnitAnimationParameters.MoveX, direction.x);
+            _animator.SetFloat(UnitAnimationParameters.MoveY, direction.y);
+        }
+        // ChangeMovingDirection is used to change the direction of the moving animation
+        public void ChangeMovingDirection(Vector2 direction)
+        {
+            _animator.SetFloat(UnitAnimationParameters.MoveX, direction.x);
+            _animator.SetFloat(UnitAnimationParameters.MoveY, direction.y);
+        }
+        // PlayAttack is used to play the attacking animation
+        public void PlayAttack()
+        {
+            _animator.SetBool(UnitAnimationParameters.IsAttacking, true);
+        }
+        // PlayIdle is used to play the idle animation
+        public void PlayIdle()
+        {
+            ResetState();
+        }
+    }
+}
