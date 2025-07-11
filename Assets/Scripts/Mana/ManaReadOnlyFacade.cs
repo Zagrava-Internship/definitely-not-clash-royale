ebaa730e762b63b97aae501dfbe3bc18b2947868
Vladyslav Yevkov <vlladevk04@gmail.com>
2025-07-11 15:36:41 +0200

    feat: add IManaReadOnly interface and ManaReadOnlyFacade for mana management

---
+++ Assets/Scripts/Mana/ManaReadOnlyFacade.cs
@@ -0,0 +1,22 @@
+﻿using UnityEngine;
+
+namespace Mana
+{
+    [RequireComponent(typeof(ManaManager))]
+    public sealed class ManaReadOnlyFacade : MonoBehaviour, IManaReadOnly
+    {
+        private ManaManager _manaManager;
+        private void Awake() => _manaManager = GetComponent<ManaManager>();
+
+        public float CurrentMana => _manaManager.currentMana;
+        public int MaxMana  => _manaManager.maxMana;
+
+        public bool CanSpend(int amount) => _manaManager.CanSpend(amount);
+
+        public event System.Action<float> OnManaChanged
+        {
+            add => _manaManager.OnManaChanged += value;
+            remove => _manaManager.OnManaChanged -= value;
+        }
+    }
+}
