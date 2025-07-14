22282ca4b8898b7efd21426f5ef021c58d146f5a
Vladdjuga <vladdjuga@gmail.com>
2025-07-14 17:15:31 +0300

    feat: update animation loop settings for handling animation delay and add WaitBeforeLoopBehaviour

---
+++ Assets/Scripts/Units/Animation/WaitBeforeLoopBehaviour.cs
@@ -0,0 +1,26 @@
+﻿using UnityEngine;
+
+namespace Units.Animation
+{
+    public class WaitBeforeLoopBehaviour: StateMachineBehaviour
+    {
+        public float delay = 1.5f;
+        private float _timer;
+        private bool _delayed;
+
+        public override void OnStateEnter(Animator animator, AnimatorStateInfo stateInfo, int layerIndex)
+        {
+            _timer = 0f;
+            _delayed = false;
+        }
+
+        public override void OnStateUpdate(Animator animator, AnimatorStateInfo stateInfo, int layerIndex)
+        {
+            if (_delayed) return;
+            _timer += Time.deltaTime;
+            if (!(_timer >= delay)) return;
+            animator.Play(stateInfo.fullPathHash, layerIndex, 0f); // Restart the animation from the beginning
+            _delayed = true;
+        }
+    }
+}
