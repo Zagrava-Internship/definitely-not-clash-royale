b272d92a9b0ea68afc0a6650629c4528a0c813b2
Vladdjuga <vladdjuga@gmail.com>
2025-07-18 16:43:40 +0300

    feat: add PrincessTower class with initial structure for future implementation

---
+++ Assets/Scripts/Units/PrincessTower.cs
@@ -0,0 +1,11 @@
+﻿using Targeting;
+using Units.UnitStates;
+using UnityEngine;
+
+namespace Units
+{
+    public class PrincessTower : MonoBehaviour
+    {
+        // TODO : Implement Princess Tower logic
+    }
+}
