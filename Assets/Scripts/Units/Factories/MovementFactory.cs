bb7ec3ea8a0dea7ab2db622a300d0c19ab0f4eef
Vladyslav Yevkov <vlladevk04@gmail.com>
2025-07-18 23:30:02 +0200

    feat: add MovementFactory and movement strategy stubs (Flying/Static)

---
+++ Assets/Scripts/Units/Factories/MovementFactory.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using Units.Enums;
+using Units.Strategies.Movement;
+using UnityEngine;
+
+namespace Units.Factories
+{
+    /// <summary>
+    /// Factory for attaching a movement strategy to a GameObject based on MovementType.
+    /// </summary>
+    public static  class MovementFactory
+    {
+        private static readonly Dictionary<MovementType, Func<GameObject, IMovementStrategy>> Map =
+            new()
+            {
+                { MovementType.Ground,  go => go.AddComponent<GroundMovement>()  }, 
+                { MovementType.Flying,  go => go.AddComponent<FlyingMovement>()  },
+                { MovementType.Static,  go => go.AddComponent<StaticMovement>()  }
+            };
+        
+        public static IMovementStrategy AddMovement(GameObject go, MovementType type)
+        {
+            if (!Map.TryGetValue(type, out var creator))
+                throw new ArgumentException($"MovementFactory: No strategy registered for {type}");
+            return creator(go);
+        }
+    }
+}
---
+++ Assets/Scripts/Units/Strategies/Movement/FlyingMovement.cs
@@ -0,0 +1,19 @@
+﻿using UnityEngine;
+
+namespace Units.Strategies.Movement
+{
+    public class FlyingMovement: MonoBehaviour, IMovementStrategy
+    {
+        public void Move(Unit unit, Vector3 destination, float speed)
+        {
+            // TODO
+            throw new System.NotImplementedException();
+        }
+
+        public void Stop(Unit unit)
+        {
+            // TODO
+            throw new System.NotImplementedException();
+        }
+    }
+}
